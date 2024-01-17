dbaefb9777faba404922c06f04feec340bf6d1f1
Ryusichan <Ryusichan@github.com>
2024-01-17 16:17:39 +0900

    first basic setting camera, obitcontrol, environment

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,32 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Environment
+        background={"only"}
+        files={"assets/textures/envmap_blur.hdr"}
+        ground={{ height: 100, radius: 300 }}
+      />
+      <Environment background={false} files={"assets/textures/envmap.hdr"} />
+      <PerspectiveCamera
+        makeDefault
+        fov={33}
+        position={[-0.09, 16.01, -27.9]}
+      />
+      <OrbitControls
+        target={[0.304, 0.806, 0.427]}
+        maxPolarAngle={Math.PI / 0.45}
+      />
+    </>
+  );
 }
 
 export default App;
