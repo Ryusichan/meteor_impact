6ae68f3bdb83bc7b6a0a5995257fec0957e6d4f8
Ryusichan <Ryusichan@github.com>
2024-01-19 18:16:30 +0900

    beams spread and move

--- src/Beams.jsx
+++ src/Beams.jsx
@@ -37,6 +37,68 @@ export default function Beams() {
 }
 
 function Beam({ geometry, beams_mask, beam_index }) {
+  const { vectors, initialPositionAttribute } = useMemo(() => {
+    let vectors = [];
+
+    const initialPositionAttribute = geometry.clone().getAttribute("position");
+
+    for (let i = 0; i < initialPositionAttribute.count; i++) {
+      let vector = new Vector3();
+      vector.fromBufferAttribute(initialPositionAttribute, i);
+
+      const idx = vectors.findIndex((vec, i) => {
+        if (vec.x == vector.x && vec.y == vector.y && vec.z == vector.z)
+          return true;
+      });
+
+      // only add the vector if it didn't already exist in the array
+      if (idx === -1) vectors.push(vector);
+    }
+
+    vectors.sort((a, b) => a.y - b.y);
+
+    return { vectors, initialPositionAttribute };
+  }, []);
+
+  useFrame((state) => {
+    const clock = state.clock;
+    const elapsed = clock.getElapsedTime();
+
+    const transformVector = new Vector3(0, 0, 1);
+    transformVector.applyAxisAngle(
+      new Vector3(0, 1, 0),
+      elapsed * 0.25 + beam_index * 17.87975
+    );
+    transformVector.multiplyScalar(3.25);
+
+    const currentPositionAttribute = geometry.getAttribute("position");
+
+    for (let i = 0; i < currentPositionAttribute.count; i++) {
+      let vector = new Vector3();
+      vector.fromBufferAttribute(initialPositionAttribute, i);
+
+      const isTopVertex =
+        (vector.x == vectors[2].x &&
+          vector.y == vectors[2].y &&
+          vector.z == vectors[2].z) ||
+        (vector.x == vectors[3].x &&
+          vector.y == vectors[3].y &&
+          vector.z == vectors[3].z);
+
+      // only add the vector if it didn't already exist in the array
+      if (isTopVertex) {
+        currentPositionAttribute.setXYZ(
+          i,
+          vector.x + transformVector.x,
+          vector.y + transformVector.y,
+          vector.z + transformVector.z
+        );
+      }
+
+      currentPositionAttribute.needsUpdate = true;
+    }
+  });
+
   const isEven = beam_index % 2 === 0;
   const color = isEven ? "#fff7ed" : "#feedd7";
   const emissive = isEven ? [0.025, 0.011, 0.01] : [0.035, 0.0195, 0.01];
